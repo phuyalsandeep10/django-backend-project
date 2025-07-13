12b0da3b5e3d3209adea57a4ca822abec1c62424
sirjan <sirjantmg99@gmail.com>
2025-07-13 16:43:32 +0545

    support redis pubsub

--- src/modules/chat/websocket/route.py
+++ src/modules/chat/websocket/route.py
@@ -5,6 +5,38 @@ import json
 from src.tasks import save_messages
 from src.models import Message
 from src.common.dependencies import get_user_by_token
+from src.config.broadcast import broadcast
+from fastapi.concurrency import run_until_first_complete
+from typing import Optional
+
+
+def get_room(conversationId:int):
+    return f"conversation-{conversationId}"
+
+async def ws_send(ws: WebSocket, conversation_id: int, user_id: Optional[int] = None):
+    try:
+        room = get_room(conversation_id)
+        async with broadcast.subscribe(channel=room) as subscriber:
+            async for event in subscriber:
+                data = json.loads(event.message)
+                if data.get('type') == 'message':
+                    print("save messages in redis subscriber")
+                    save_messages.delay(
+                        conversation_id=conversation_id,
+                        data={"message": data.get('message')},
+                        user_id=user_id
+                    )
+                await ws.send_text(event.message)
+    except Exception as e:
+        print(f"Exception in ws_send: {e}")
+
+async def ws_recv(ws: WebSocket, conversation_id: int, user_id: Optional[int] = None):
+    try:
+        room = get_room(conversation_id)
+        async for text in ws.iter_text():
+            await broadcast.publish(channel=room, message=text)
+    except Exception as e:
+        print(f"Exception in ws_recv: {e}")
 
 @app.websocket("/ws/{conversation_id}")
 async def ws_room(websocket: WebSocket, conversation_id: int,token:str = Query(None)):
@@ -14,6 +46,8 @@ async def ws_room(websocket: WebSocket, conversation_id: int,token:str = Query(N
     room = rooms.setdefault(conversation_id, Room())
 
     await room.connect(websocket, role)
+
+    
 
     user_id = None
 
@@ -25,25 +59,31 @@ async def ws_room(websocket: WebSocket, conversation_id: int,token:str = Query(N
         role = 'user'
         user_id = user.id
 
+    
 
     try:
-        while True:
-            data = await websocket.receive_text()
-            data = json.loads(data)
+        await run_until_first_complete(
+            (ws_recv, {"ws": websocket, "conversation_id": conversation_id, "user_id": user_id}),
+            (ws_send, {"ws": websocket, "conversation_id": conversation_id, "user_id": user_id}),
+        )
+        # while True:
+        #     data = await websocket.receive_text()
+        #     data = json.loads(data)
 
            
-            if data.get('type') =='message':
-                print("save messages in websocket")
-                save_messages.delay(
-                conversation_id=conversation_id,
-                data={
-                    "message":data.get('message')
-                },
-                user_id=user_id)
-            await room.broadcast(role, data)
+        #     if data.get('type') =='message':
+        #         print("save messages in websocket")
+        #         save_messages.delay(
+        #         conversation_id=conversation_id,
+        #         data={
+        #             "message":data.get('message')
+        #         },
+        #         user_id=user_id)
+        #     await room.broadcast(role, data)
 
     except WebSocketDisconnect:
         room.disconnect(websocket)
     except Exception as e:
         print(f"WebSocket error: {e}")
         room.disconnect(websocket)
+
