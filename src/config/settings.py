56d19926240959ca07e629b503b9f3c2ea818b88
aryantmg628 <aryantmg628@gmail.com>
2025-07-15 10:29:07 +0545

    refactor : refactor the code

--- src/config/settings.py
+++ src/config/settings.py
@@ -12,13 +12,21 @@ class Settings(BaseSettings):
     API_PREFIX: str
     ALLOWED_HOSTS: List[str] = ["*"]
     SECRET_KEY: str
-    ALGORITHM: str
+    ALGORITHM: str = "H256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     KAFKA_BOOTSTRAP_SERVERS: str
     KAFKA_TOPIC: str
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
+    SMTP_SERVER: str
+    SMTP_PORT: int
+    SMTP_USERNAME: str
+    SMTP_PASSWORD: str
+    GOOGLE_CLIENT_ID: int
+    GOOGLE_CLIENT_SECRET: str
+    FRONTEND_URL: str
+    CELEREY_BROKER_URL: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
