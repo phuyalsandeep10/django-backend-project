4f27ae199e6848fe46218264d2c5e1a92a9608d0
aryantmg628 <aryantmg628@gmail.com>
2025-07-15 10:19:11 +0545

    feat : BaseSettings added for dealing with environment variables

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,30 +1,26 @@
-PROJECT_NAME = "MyProject"
-PROJECT_VERSION = "1.0.0"
-PROJECT_DESCRIPTION = "This is a sample project to demonstrate configuration settings."
-DATABASE_URL = "sqlite:///./test.db"
-API_PREFIX = "/api"
-ALLOWED_HOSTS = ["*"]
-SECRET_KEY = "your-secret-key"
+from typing import List
 
-import os
-
-# Use 'kafka:9092' if running in Docker, else 'localhost:29092'
-KAFKA_BOOTSTRAP_SERVERS = os.getenv(
-    "KAFKA_BOOTSTRAP_SERVERS",
-    "kafka:9092" if os.getenv("IN_DOCKER") else "localhost:29092",
-)
-KAFKA_TOPIC = "chat-messages"
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class settings:
-    PROJECT_NAME: str = PROJECT_NAME
-    PROJECT_VERSION: str = PROJECT_VERSION
-    PROJECT_DESCRIPTION: str = PROJECT_DESCRIPTION
-    DATABASE_URL: str = DATABASE_URL
-    API_PREFIX: str = API_PREFIX
-    ALLOWED_HOSTS: list[str] = ALLOWED_HOSTS
-    SECRET_KEY: str = SECRET_KEY
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15  # 15 minutes
-    KAFKA_BOOTSTRAP_SERVERS: str = KAFKA_BOOTSTRAP_SERVERS
-    KAFKA_TOPIC: str = KAFKA_TOPIC
+class Settings(BaseSettings):
+
+    PROJECT_NAME: str
+    PROJECT_VERSION: str
+    PROJECT_DESCRIPTION: str
+    DATABASE_URL: str
+    API_PREFIX: str
+    ALLOWED_HOSTS: List[str] = ["*"]
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    KAFKA_BOOTSTRAP_SERVERS: str
+    KAFKA_TOPIC: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
