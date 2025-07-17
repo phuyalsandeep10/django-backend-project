d887aa5d3fc5a9327d665b8aadbec2d1bf6363a6
aryantmg628 <aryantmg628@gmail.com>
2025-07-17 15:25:41 +0545

    feat : to_dict method added

--- src/modules/auth/models.py
+++ src/modules/auth/models.py
@@ -1,22 +1,24 @@
-from typing import Optional, TYPE_CHECKING, List
-from sqlmodel import Field, Relationship
 from datetime import datetime
+from typing import TYPE_CHECKING, List, Optional
+
 import sqlalchemy as sa
+from sqlmodel import Field, Relationship
+
 from src.common.models import BaseModel
-from src.modules.organizations.models import OrganizationMember
-from src.modules.team.models import TeamMember
 from src.modules.chat.models.conversation import ConversationMember
 from src.modules.chat.models.message import Message
-
+from src.modules.organizations.models import OrganizationMember
+from src.modules.team.models import TeamMember
+from src.modules.ticket.models.ticket import Ticket, TicketAssigneesLink
 
 
 class User(BaseModel, table=True):
-    __tablename__ = 'sys_users' #type:ignore
+    __tablename__ = "sys_users"  # type:ignore
     email: str = Field(unique=True, index=True)
     name: Optional[str] = None
     image: Optional[str] = None
     mobile: Optional[str] = Field(default=None, unique=True)
-    password: str = Field(default='', min_length=8, max_length=128)
+    password: str = Field(default="", min_length=8, max_length=128)
     is_active: bool = Field(default=True)
     email_verified_at: datetime = Field(
         default=None,
@@ -47,25 +49,25 @@ class User(BaseModel, table=True):
         sa_relationship_kwargs={"foreign_keys": "[ConversationMember.user_id]"},
     )
 
+    assigned_tickets: List[Ticket] = Relationship(
+        back_populates="assignees", link_model=TicketAssigneesLink
+    )
 
-
-    
-
+    def to_dict(self):
+        return {"email": self.email, "name": self.name}
 
 
 class RefreshToken(BaseModel, table=True):
-    __tablename__ = 'refresh_tokens' #type:ignore
+    __tablename__ = "refresh_tokens"  # type:ignore
     user_id: int = Field(foreign_key="sys_users.id")
     token: str = Field(unique=True, index=True)
     active: bool = Field(default=False)
     created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
     expires_at: datetime = Field(nullable=False)
 
-    
-
 
 class EmailVerification(BaseModel, table=True):
-    __tablename__ = 'email_verifications' #type:ignore
+    __tablename__ = "email_verifications"  # type:ignore
     user_id: int = Field(foreign_key="sys_users.id")
     token: str = Field(unique=True, index=True)
     is_used: bool = Field(default=False)
@@ -74,5 +76,3 @@ class EmailVerification(BaseModel, table=True):
         nullable=True
     )  # Use appropriate datetime type if needed
     type: str = Field(default="email_verification")
-
-   
