e9892ca6faac479f19a463b7c91c555b316239b9
aryantmg628 <aryantmg628@gmail.com>
2025-07-17 09:46:58 +0545

    feat : to_dict method added to get data in dict

--- src/modules/ticket/models/sla.py
+++ src/modules/ticket/models/sla.py
@@ -11,12 +11,22 @@ from .ticket import Ticket
 class SLA(BaseModel, table=True):
     __tablename__: str = "sla"  # type: ignore
 
-    name: str = Field(nullable=True)
+    name: str = Field(nullable=True, unique=True)
     response_time: int = Field(nullable=False)
     resolution_time: int = Field(nullable=False)
     issued_by: int = Field(nullable=False)
     created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
     tickets: List[Ticket] = Relationship(back_populates="sla")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "response_time": self.response_time,
+            "resolution_time": self.resolution_time,
+            "issued_by": self.issued_by,
+            "created_at": self.created_at.isoformat(),
+        }
 
     def __str__(self):
         return f"{self.name}-{self.created_at}"
