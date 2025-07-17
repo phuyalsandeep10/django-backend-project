c4a7a0398b73113bb5f3bc259c9b1305adb472b2
aryantmg628 <aryantmg628@gmail.com>
2025-07-17 09:47:23 +0545

    feat : list all sla routes added

--- src/modules/ticket/routers/sla.py
+++ src/modules/ticket/routers/sla.py
@@ -13,3 +13,8 @@ async def register_sla(
     payload: CreateSLASchema, authorization: Annotated[str, Header()]
 ):
     return await sla_service.register_sla(payload, authorization)
+
+
+@router.get("/sla", summary="List all Service Level agreements")
+async def get_all():
+    return await sla_service.get_all_sla()
