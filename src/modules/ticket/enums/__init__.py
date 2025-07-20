dece7f8a8eb8a3e0a19d0635365e784abf3e811b
aryantmg628 <aryantmg628@gmail.com>
2025-07-20 11:53:37 +0545

    feat : ticket alert warning level enums added

--- src/modules/ticket/enums/__init__.py
+++ src/modules/ticket/enums/__init__.py
@@ -13,3 +13,14 @@ class StatusEnum(str, Enum):
     OPEN = "open"
     PENDING = "pending"
     CLOSED = "closed"
+
+
+class WarningLevelEnum(str, Enum):
+    WARNING_75 = 75
+    WARNING_90 = 90
+    WARNING_100 = 100
+
+
+class TicketAlertTypeEnum(str, Enum):
+    RESPONSE = "response"
+    RESOLUTION = "resolution"
