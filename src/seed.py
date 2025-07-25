deeb260d1d888bf838a7d01b998656ca04de224b
aryantmg628 <aryantmg628@gmail.com>
2025-07-25 12:17:54 +0545

    feat : priority, organization, user seed added

--- src/seed.py
+++ src/seed.py
@@ -3,10 +3,27 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
 import src.models
 from src.common.utils import hash_password
 from src.modules.auth.models import User
 from src.modules.organizations.models import Organization
+from src.modules.ticket.models import Priority, TicketStatus
+
+# default data
+priorities = [
+    {"name": "Critical", "level": 0, "color": "#ffffff", "organization_id": 1},
+    {"name": "High", "level": 1, "color": "#ffffff", "organization_id": 1},
+    {"name": "Medium", "level": 2, "color": "#ffffff", "organization_id": 1},
+    {"name": "Low", "level": 3, "color": "#ffffff", "organization_id": 1},
+    {"name": "Trivial", "level": 4, "color": "#ffffff", "organization_id": 1},
+]
+
+status = [
+    {"name": "Open", "color": "#ffffff", "organization_id": 1},
+    {"name": "Pending", "color": "#ffffff", "organization_id": 1},
+    {"name": "Closed", "color": "#ffffff", "organization_id": 1},
+]
 
 
 async def test_user_seed():
@@ -21,7 +38,8 @@ async def test_user_seed():
             password=hash_password(data["password"]),
         )
         print("Test User created")
-    print("Test User already exists")
+    else:
+        print("Test User already exists")
 
 
 async def test_organization():
@@ -45,12 +63,47 @@ async def test_organization():
 
 
 async def priority_seed():
-    pass
+    for i in priorities:
+        record = await Priority.find_one(
+            where={
+                "name": {"mode": "insensitive", "value": i["name"]},
+            }
+        )
+        if not record:
+            await Priority.create(
+                name=i["name"],
+                level=i["level"],
+                color=i["color"],
+                organization_id=i["organization_id"],
+            )
+            print("Priority created")
+        else:
+            print("Priority already created")
+
+
+async def status_seed():
+    for i in status:
+        record = await TicketStatus.find_one(
+            where={
+                "name": {"mode": "insensitive", "value": i["name"]},
+            }
+        )
+        if not record:
+            await TicketStatus.create(
+                name=i["name"],
+                color=i["color"],
+                organization_id=i["organization_id"],
+            )
+            print("Status created")
+        else:
+            print("Status already created")
 
 
 async def seed_func():
     await test_organization()
-    # await test_user_seed()
+    await test_user_seed()
+    await priority_seed()
+    await status_seed()
 
 
 if __name__ == "__main__":
