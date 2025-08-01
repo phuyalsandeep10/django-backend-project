7b9247fd087e9b9f3948159b212cc56295512a39
aryantmg628 <aryantmg628@gmail.com>
2025-08-01 14:19:46 +0545

    refactor : refactor the code

--- src/config/settings.py
+++ src/config/settings.py
@@ -40,11 +40,9 @@ class Settings(BaseSettings):
     FRONTEND_URL: str = "http://localhost:3000"
     CELEREY_BROKER_URL: str = "redis://localhost:6379"
 
-    CLOUDINARY_CLOUD_NAME:str = ''
-    CLOUDINARY_API_KEY:str = ''
-    CLOUDINARY_API_SECRET:str = ''
-
-
+    CLOUDINARY_CLOUD_NAME: str = ""
+    CLOUDINARY_API_KEY: str = ""
+    CLOUDINARY_API_SECRET: str = ""
 
     model_config = SettingsConfigDict(env_file=".env")
 
