3211eabf125c0b4fa2d5038a6fc78960e17a920a
aryantmg628 <aryantmg628@gmail.com>
2025-08-03 12:53:51 +0545

    feat : bulk insert for default values

--- migrations/versions/20250803_044729_ticket_priority_table_added.py
+++ migrations/versions/20250803_044729_ticket_priority_table_added.py
@@ -8,7 +8,10 @@ Create Date: 2025-08-03 10:32:30.150870
 
 from typing import Sequence, Union
 
+from alembic import op
+
 from migrations.base import BaseMigration
+from src.modules.ticket.models.priority import TicketPriority
 
 revision: str = "20250803_044729"
 down_revision: Union[str, Sequence[str], None] = "20250801_091606"
@@ -41,6 +44,44 @@ def upgrade() -> None:
     Function to create a table
     """
     TicketPriorityMigration().upgrade()
+    default_priorities = [
+        {
+            "id": 1,
+            "name": "critical",
+            "level": "0",
+            "bg_color": "#FAD6D5",
+            "fg_color": "#F61818",
+            "organization_id": None,
+        },
+        {
+            "id": 2,
+            "name": "high",
+            "level": "1",
+            "bg_color": "#FFF0D2",
+            "fg_color": "#F5CE31",
+            "organization_id": None,
+        },
+        {
+            "id": 3,
+            "name": "medium",
+            "level": "2",
+            "bg_color": "#DAE8FA",
+            "fg_color": "#3872B7",
+            "organization_id": None,
+        },
+        {
+            "id": 4,
+            "name": "low",
+            "level": "3",
+            "bg_color": "#E5F9DB",
+            "fg_color": "#009959",
+            "organization_id": None,
+        },
+    ]
+    TicketPriorityMigration().bulk_insert_data(rows=default_priorities)
+    op.execute(
+        "SELECT setval('ticket_priority_id_seq', (SELECT MAX(id) FROM ticket_priority))"
+    )
 
 
 def downgrade() -> None:
--- migrations/versions/20250803_050421_ticket_status_table_added.py
+++ migrations/versions/20250803_050421_ticket_status_table_added.py
@@ -8,6 +8,8 @@ Create Date: 2025-08-03 10:49:21.461892
 
 from typing import Sequence, Union
 
+from alembic import op
+
 from migrations.base import BaseMigration
 
 revision: str = "20250803_050421"
@@ -41,6 +43,45 @@ def upgrade() -> None:
     Function to create a table
     """
     TicketStatusMigration().upgrade()
+    default_ticket_status = [
+        {
+            "id": 1,
+            "name": "Unassigned",
+            "bg_color": "#F61818",
+            "fg_color": "#ffffff",
+            "organization_id": None,
+            "status_category": "pending",
+        },
+        {
+            "id": 2,
+            "name": "Assigned",
+            "bg_color": "#FFF0D2",
+            "fg_color": "#ffffff",
+            "organization_id": None,
+            "status_category": "open",
+        },
+        {
+            "id": 3,
+            "name": "Solved",
+            "bg_color": "#009959",
+            "fg_color": "#ffffff",
+            "organization_id": None,
+            "status_category": "closed",
+        },
+        {
+            "id": 4,
+            "name": "Reopened",
+            "bg_color": "#DAE8FA",
+            "fg_color": "#ffffff",
+            "organization_id": None,
+            "status_category": "open",
+        },
+    ]
+    TicketStatusMigration().bulk_insert_data(rows=default_ticket_status)
+
+    op.execute(
+        "SELECT setval('ticket_status_id_seq', (SELECT MAX(id) FROM ticket_status))"
+    )
 
 
 def downgrade() -> None:
