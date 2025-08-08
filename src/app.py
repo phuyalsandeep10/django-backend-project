bddf954629b2b237a0a8dcc4323e3cb8c235eb3b
sirjan <sirjantmg99@gmail.com>
2025-08-08 11:03:10 +0000

    done

--- src/app.py
+++ src/app.py
@@ -19,7 +19,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_HOSTS,
+    allow_origins= ['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
