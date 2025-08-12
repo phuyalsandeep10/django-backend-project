58377d9b5841a47099d70ab14a13cac601a5fa51
Aryan Tamang <104264310+AryanTmg628@users.noreply.github.com>
2025-08-12 17:18:19 +0545

    feat : authmiddleware added

--- src/app.py
+++ src/app.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
+from src.common.dependencies import get_user_by_token
 from src.config.broadcast import broadcast
 from src.config.settings import settings
+from src.middleware import AuthMiddleware
 
 # Replace with your friend's IP or use "*" for all origins (less secure)
 
@@ -19,10 +21,16 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= ['*'],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
+# Auth middleware
+app.add_middleware(
+    AuthMiddleware,
+    get_user_by_token,
+    extemp_paths=["/auth/login", "/docs", "/openapi.json", "/auth/me", "/auth/logout"],
 )
 
 # Session middleware for OAuth
