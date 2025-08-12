ab7b60dbf67ee4c7bff32cef05f2a3621a1370f2
Aryan Tamang <104264310+AryanTmg628@users.noreply.github.com>
2025-08-12 17:18:07 +0545

    feat : auth middleware added for organization

---
+++ src/middleware/__init__.py
@@ -0,0 +1,62 @@
+import logging
+from typing import List, Optional
+
+from src.common.context import TenantContext, UserContext
+from src.utils.response import CustomResponse as cr
+
+logger = logging.getLogger(__name__)
+
+
+from fastapi import HTTPException, Request, status
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import Response
+
+
+class AuthMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app, get_user_by_token, extemp_paths: Optional[List[str]]):
+        super().__init__(app)
+        self.get_user_by_token = (
+            get_user_by_token  # async function to get user from token
+        )
+        self.extemp_paths = extemp_paths
+
+    async def dispatch(self, request: Request, call_next):
+
+        # skip protected or extempted paths
+        if self.extemp_paths and any(
+            request.url.path.startswith(path) for path in self.extemp_paths
+        ):
+            return await call_next(request)
+
+        try:
+            auth_header = request.headers.get("Authorization")
+            if not auth_header or not auth_header.startswith("Bearer "):
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Missing or invalid Authorization header",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            token = auth_header.split(" ")[1]
+            user = await self.get_user_by_token(token)
+            if not user:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Invalid token or user not found",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            UserContext.set(user.id)
+            organization_id = user.attributes.get("organization_id")
+            if organization_id:
+                TenantContext.set(organization_id)
+            else:
+                TenantContext.set(None)
+
+            response = await call_next(request)
+            return response
+
+        except Exception as e:
+            return cr.error(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                message="Authentication required",
+                data=str(e),
+            )
