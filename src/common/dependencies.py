18f5ecd02c251a5d3dd07c425e77c799b82870f4
Sirjan Tamang <56628702+sirjandrn25@users.noreply.github.com>
2025-08-17 10:30:16 +0545

    credentials done

--- src/common/dependencies.py
+++ src/common/dependencies.py
@@ -42,11 +42,13 @@ async def validate_user(
 ):
     """Get current authenticated user"""
 
+    print("couldn't validate user")
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
+    
 
     try:
         token = (
