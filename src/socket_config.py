99f275fe0a28cf262cde3f2d880d75afbc44b404
Sirjan Tamang <56628702+sirjandrn25@users.noreply.github.com>
2025-08-18 11:23:34 +0545

    remove conflict

--- src/socket_config.py
+++ src/socket_config.py
@@ -1,5 +1,4 @@
 import socketio
-<<<<<<< HEAD
 from src.websocket.chat_namespaces.customer_chat_namespace import CustomerChatNamespace
 from src.websocket.chat_namespaces.agent_chat_namespace import AgentChatNamespace
 from src.config.redis.redis_listener import redis_listener
@@ -10,12 +9,10 @@ from socketio import AsyncRedisManager
 redis_url = settings.REDIS_URL
 mgr = AsyncRedisManager(redis_url)
 
-=======
 
 from src.app import app
 from src.modules.chat.websocket import ChatNamespace
 from src.modules.ticket.websocket.sla_websocket import AlertNameSpace
->>>>>>> main
 
 # Create the Socket.IO Async server (ASGI mode)
 sio = socketio.AsyncServer(
@@ -31,7 +28,6 @@ socket_app = socketio.ASGIApp(
 )
 
 
-<<<<<<< HEAD
 sio.register_namespace(CustomerChatNamespace())
 sio.register_namespace(AgentChatNamespace())
 
@@ -81,9 +77,3 @@ async def stop_ws_redis_listener():
             print("✅ Redis listener task cancelled")
         except Exception as e:
             print(f"⚠️ Error stopping Redis listener: {e}")
-=======
-alert_ns = AlertNameSpace("/alert")
-
-sio.register_namespace(ChatNamespace())
-sio.register_namespace(alert_ns)
->>>>>>> main
