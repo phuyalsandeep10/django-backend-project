aeb2a9d9f3052bc97cc52cfe97fb1dfb76e50f33
Aryan Tamang <104264310+AryanTmg628@users.noreply.github.com>
2025-08-18 11:10:26 +0545

    feat : run scheduler independently

--- src/config/scheduler.py
+++ src/config/scheduler.py
@@ -1,10 +1,27 @@
 import asyncio
+import logging
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
+from src.tasks.sla_task import check_sla_breach
+
+logger = logging.getLogger("my_scheduler")
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()  # print to stdout
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 scheduler = AsyncIOScheduler()
 
 
-def start_scheduler():
+async def start_scheduler():
+    scheduler.add_job((check_sla_breach), IntervalTrigger(seconds=10))
     scheduler.start()
+
+    await asyncio.Event().wait()
+
+
+if __name__ == "__main__":
+    asyncio.run(start_scheduler())
