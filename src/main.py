b6f98796ad949936bdd1d74cfe0797541f86d173
Aryan Tamang <104264310+AryanTmg628@users.noreply.github.com>
2025-08-18 11:10:06 +0545

    refactor : removed apscheduler to run with it

--- src/main.py
+++ src/main.py
@@ -1,20 +1,14 @@
-import asyncio
 import logging
 
-from apscheduler.triggers.interval import IntervalTrigger
 from fastapi import Request, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from src.app import app
-from src.config.arq import WorkerSettings
 from src.config.broadcast import broadcast
-from src.config.scheduler import scheduler, start_scheduler
 from src.routers import add_routers
 from src.socket_config import socket_app
-from src.tasks.sla_task import check_sla_breach
 from src.utils.exceptions import add_exceptions_handler
-from src.utils.get_templates import get_templates
 
 # custom exceptions
 add_exceptions_handler(app)
@@ -23,13 +17,6 @@ add_exceptions_handler(app)
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s -%(name)s - %(message)s"
 )
-
-
-# apscheduler configuration
-@app.on_event("startup")
-async def startup_event():
-    start_scheduler()
-    scheduler.add_job(check_sla_breach, IntervalTrigger(seconds=10))
 
 
 # adding routers
